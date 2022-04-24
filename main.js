4e86bfae2c3c8b9980ca6b95b2509fa28a15504b
igorka1996 <79985703+igorka1996@users.noreply.github.com>
2022-04-24 18:50:51 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const rand = document.querySelector('.rand');
+const hone = document.querySelector('.hone');
+const pisa = document.querySelector('.pisa');
+const popa = document.querySelector('.popa');
+const soset = document.querySelector('.soset');
+rand.addEventListener('click', () => {
+   let chislo = Math.floor(Math.random() * 14);
+   if (chislo > 4 && chislo < 10) {
+      popa.classList.add('active');
+   } else if(chislo > 9){
+      pisa.classList.add('active');
+   } else {
+      soset.classList.add('active');
+   }
+   setTimeout(() => {
+      popa.classList.remove('active');
+      pisa.classList.remove('active');
+      soset.classList.remove('active');
+   }, 1000)
+});
+
+
